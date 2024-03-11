ff0160d7f5aed407917f1dbeb9a4e98437af9d68
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2024-03-11 15:45:11 -0300

    Ajustes+botaoadc

--- JS/index.js
+++ JS/index.js
@@ -35,3 +35,29 @@ function closeModal(){
     document.getElementById("modalContainer").style.opacity = "0";
     document.getElementById("modalOverlay").style.opacity = "0";
 }
+
+// Limitar seleção de imagens para > 800x600px
+document.getElementById('seletorArquivos').addEventListener('change', function(event) {
+    var input = event.target;
+    var files = input.files;
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var img = new Image();
+
+        img.onload = function() {
+            // Verificar se a largura e altura da imagem atendem aos requisitos
+            if (img.width >= 700 && img.height >= 500) {
+                // A imagem atende aos requisitos, você pode realizar ações adicionais aqui
+                console.log('Imagem válida:', file.name);
+            } else {
+                // A imagem não atende aos requisitos, você pode exibir uma mensagem de erro
+                alert('A imagem ' + file.name + ' não atende aos requisitos de resolução (mínimo: 800x600).');
+                // Remover o arquivo da lista de seleção
+                input.value = '';
+            }
+        };
+
+        img.src = URL.createObjectURL(file);
+    }
+});
